b719a2c373cf3f9c85b85021aabfccea9eee8b8a
Ali Kaan TURKMEN <turkmenalikaan@gmail.com>
2020-04-13 02:44:35 +0300

    [add]: singleton, factory method, abstract factory

---
+++ FactoryMethod/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager(new LoggerFactory());
+            customerManager.Save();
+            customerManager = new CustomerManager(new LoggerFactory2());
+            customerManager.Save();
+
+            Console.ReadLine();
+        }
+    }
+
+    public class LoggerFactory:ILoggerFactory
+    {
+        public ILogger CreateLogger()
+        {
+            // Business  to deicede factory 
+            return new MailLogger();
+        }
+    }
+
+    public class LoggerFactory2 : ILoggerFactory
+    {
+        public ILogger CreateLogger()
+        {
+            // Business  to deicede factory 
+            return new NetworkLogger();
+        }
+    }
+
+    public interface ILoggerFactory
+    {
+        ILogger CreateLogger();
+    }
+
+    public interface ILogger
+    {
+        void Log();
+    }
+
+    public class MailLogger : ILogger
+    {
+
+        public void Log()
+        {
+            Console.WriteLine("Logged with MailLogger");
+        }
+    }
+
+    public class NetworkLogger : ILogger
+    {
+
+        public void Log()
+        {
+            Console.WriteLine("Logged with NetworkLogger");
+        }
+    }
+
+    public class CustomerManager
+    {
+        private ILoggerFactory _loggerFactory;
+
+        public CustomerManager(ILoggerFactory loggerFactory)
+        {
+            _loggerFactory = loggerFactory;
+        }        
+
+        public void Save()
+        {
+            Console.WriteLine("Saved!");
+            ILogger logger = _loggerFactory.CreateLogger();
+            logger.Log();
+        }
+    }
+}
---
+++ Singleton/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var customerManager = CustomManager.CreateAsSingleton();            
+            customerManager.Save();
+
+            customerManager = CustomManager.CreateAsSingleton();            
+            customerManager.Save();
+
+            Console.ReadLine();
+        }
+    }
+    
+    class CustomManager
+    {
+        private static CustomManager _customManager;    // define as static to make it singleton
+        static object _lockObject = new object();
+        private CustomManager()     // private constructer, so can't new this 
+        {
+
+        }
+
+        public static CustomManager CreateAsSingleton()
+        {            
+            lock(_lockObject)
+            {
+                if(_customManager == null)
+                {
+                    _customManager = new CustomManager();
+                }
+            }
+            return _customManager;
+        }
+
+        public void Save()
+        {
+            Console.WriteLine("Saved");
+        }
+    }
+
+}
