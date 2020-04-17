b1b6d1a88c164d1de3575f61018c361f2082ac90
Ali Kaan TURKMEN <turkmenalikaan@gmail.com>
2020-04-17 23:11:30 +0300

    [add]: prototype, builder, fascade, adapter

---
+++ Builder/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductDirector director = new ProductDirector();
+            var builderNew = new NewCustomerProductBuilder();
+            var builderOld = new OldCustomerProductBuilder();
+
+            director.GenerateProduct(builderNew);
+            var model = builderNew.GetModel();
+
+            Console.WriteLine(model.Id);
+            Console.WriteLine(model.CategoryName);                        
+            Console.WriteLine(model.DiscountApplied);
+            Console.WriteLine(model.DiscountedPrice);
+            Console.WriteLine(model.ProductName);
+            Console.WriteLine(model.UnitPrice);
+
+            director.GenerateProduct(builderOld);
+            model = builderOld.GetModel();
+
+            Console.WriteLine(model.Id);
+            Console.WriteLine(model.CategoryName);
+            Console.WriteLine(model.DiscountApplied);
+            Console.WriteLine(model.DiscountedPrice);
+            Console.WriteLine(model.ProductName);
+            Console.WriteLine(model.UnitPrice);
+
+            Console.ReadLine();
+        }
+    }
+
+    class ProductViewModel
+    {
+        public int Id { get; set; }
+        public string CategoryName { get; set; }
+        public string ProductName { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal DiscountedPrice { get; set; }
+        public bool DiscountApplied { get; set; }
+    }
+
+    abstract class ProductBuilder
+    {
+        public abstract void GetProductData();
+        public abstract void ApplyDiscound();
+        public abstract ProductViewModel GetModel();
+    }
+
+    class NewCustomerProductBuilder : ProductBuilder
+    {
+        ProductViewModel model = new ProductViewModel();
+
+        public override void GetProductData()
+        {
+            model.Id = 1;
+            model.CategoryName = "Beverages";
+            model.ProductName = "Chai";
+            model.UnitPrice = 20;
+            
+        }
+        public override void ApplyDiscound()
+        {
+            model.DiscountedPrice = model.UnitPrice * (decimal)0.90;
+            model.DiscountApplied = true;
+        }
+
+        public override ProductViewModel GetModel()
+        {
+            return model;
+        }
+
+        
+    }
+
+    class OldCustomerProductBuilder : ProductBuilder
+    {
+        ProductViewModel model = new ProductViewModel();
+
+        public override void GetProductData()
+        {
+            model.Id = 1;
+            model.CategoryName = "Beverages";
+            model.ProductName = "Chai";
+            model.UnitPrice = 20;
+        }
+        public override void ApplyDiscound()
+        {
+            model.DiscountedPrice = model.UnitPrice;
+            model.DiscountApplied = false;            
+        }
+
+        public override ProductViewModel GetModel()
+        {
+            return model;
+        }        
+    }
+
+    class ProductDirector
+    {
+        public void GenerateProduct(ProductBuilder productBuilder)
+        {
+            productBuilder.GetProductData();
+            productBuilder.ApplyDiscound();
+        }
+    }
+}
--- Prototype/Program.cs
+++ Prototype/Program.cs
@@ -10,6 +10,42 @@ namespace Prototype
     {
         static void Main(string[] args)
         {
+            Customer customer1 = new Customer { FirstName = "Ali Kaan", LastName = "Türkmen", City = "İzmir", Id = 1 };
+
+            Customer customer2 = (Customer)customer1.Clone();
+            customer2.FirstName = "İsmail";           
+
+            Console.WriteLine(customer1.FirstName + " " + customer1.LastName + " " + customer1.City);
+            Console.WriteLine(customer2.FirstName + " " + customer2.LastName + " " + customer2.City);
+
+            Console.ReadLine();
+        }
+    }
+
+    public abstract class Person
+    {
+        public abstract Person Clone();
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+    public class Customer : Person
+    {
+        public string City { get; set; }
+
+        public override Person Clone()
+        {
+            return (Person)MemberwiseClone();
+        }
+    }
+
+    public class Employee : Person
+    {
+        public decimal Salary { get; set; }
+
+        public override Person Clone()
+        {
+            return (Person)MemberwiseClone();
         }
     }
 }
