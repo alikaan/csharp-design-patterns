b20cf2df455edaf670fd5fed050dd324fd17ffb0
Ali Kaan TURKMEN <turkmenalikaan@gmail.com>
2020-04-25 23:09:51 +0300

    [add]: proxy, decorator, bridge

---
+++ Brdige/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Brdige
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            CustomerManager customerManager = new CustomerManager();
+
+            customerManager.MessageSenderBase = new EMailSender();
+
+            customerManager.UpdateCustomer();
+
+            Console.ReadLine();
+        }
+    }
+
+    abstract class MessageSenderBase
+    {
+        public void Save()
+        {
+            Console.WriteLine("Message saved!");
+        }
+
+        public abstract void Send(Body body);        
+    }
+
+    class Body
+    {
+        public string Title { get; set; }
+        public string Text { get; set; }
+    }
+
+    class EMailSender : MessageSenderBase
+    {
+        public override void Send(Body body)
+        {
+            Console.WriteLine("{0} was sent via EMail Sender", body.Title);
+        }
+    }   
+    class SmsSender : MessageSenderBase
+    {
+        public override void Send(Body body)
+        {
+            Console.WriteLine("{0} was sent via SMS Sender", body.Title);
+        }
+    }
+
+    class CustomerManager
+    {
+        public MessageSenderBase MessageSenderBase { get; set; }
+        public void UpdateCustomer()
+        {
+            MessageSenderBase.Send(new Body { Title= "Bridge Design Pattern"});
+            Console.WriteLine("Customer Updated!");
+        }
+    }    
+
+}
---
+++ Decorator/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var personalCar = new PersonalCar { Make = "Hyundai", Model = "i20", HirePrice = 2100 };
+
+            SpecialOffer specialOffer = new SpecialOffer(personalCar);
+            specialOffer.DiscountPercantage = 10;
+
+
+            Console.WriteLine("Concrete : {0}", personalCar.HirePrice);
+            Console.WriteLine("Special offer : {0}", specialOffer.HirePrice);
+
+            Console.ReadLine();
+        }
+    }
+
+    abstract class CarBase
+    {
+        public abstract string Make { get; set; }
+        public abstract string Model { get; set; }
+        public abstract decimal HirePrice { get; set; }
+    }
+
+    class PersonalCar : CarBase
+    {
+        public override string Make { get; set; }
+        public override string Model { get; set; }
+        public override decimal HirePrice { get; set; }
+    }
+
+    class CommerialCar : CarBase
+    {
+        public override string Make { get; set; }
+        public override string Model { get; set; }
+        public override decimal HirePrice { get; set; }
+    }
+
+    abstract class CarDecoratorBase : CarBase
+    {
+        private CarBase _carbase;
+
+        protected CarDecoratorBase(CarBase carBase)
+        {
+            _carbase = carBase;
+        }
+    }
+
+    class SpecialOffer : CarDecoratorBase
+    {
+        public int DiscountPercantage { get; set; }
+        private readonly CarBase _carBase;
+
+        public SpecialOffer(CarBase carBase) : base(carBase)
+        {
+            _carBase = carBase;
+        }
+
+        public override string Make { get; set; }
+        public override string Model { get; set; }
+        public override decimal HirePrice {
+
+            get {   return _carBase.HirePrice  - (_carBase.HirePrice * DiscountPercantage/100); }            
+            set
+            {   
+            } 
+        }
+    }
+    class RamazanOffer : CarDecoratorBase
+    {
+
+        public int DiscountPercantage { get; set; }
+
+        private readonly CarBase _carBase;
+        public RamazanOffer(CarBase carBase) : base(carBase)
+        {
+            _carBase = carBase;
+        }
+
+        public override string Make { get; set; }
+        public override string Model { get; set; }
+        public override decimal HirePrice { 
+            get {   return _carBase.HirePrice - (_carBase.HirePrice * DiscountPercantage / 100); }
+            set 
+            {
+            } 
+        }
+    }
+
+
+}
